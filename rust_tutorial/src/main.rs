c87dc20ab4bdce043c2039b9bde76cf47e49e2c3
Armin Ashrafi <ArminneAshrafi@gmail.com>
2023-04-14 15:22:40 +0430

    learned the hello world program

--- rust_tutorial/src/main.rs
+++ rust_tutorial/src/main.rs
@@ -2,7 +2,19 @@
 
 use std::io;
 use rand::Rng;
+use std::io::{Write,BufReader,BufRead,ErrorKind};
+use std::fs::File;
+
+use std::cmp::Ordering;
 
 fn main() {
-    println!("Hello, world!");
+    println!("What is your name");
+    let mut name: String = String::new(); //the :: access the methods of a type, 
+                                             //without the need to have an instance of it
+    let greeting: &str = "Nice to meet you!";
+
+    io::stdin().read_line(&mut name).expect("Failed to read line"); // We will make name mutable
+    
+
+    println!("Hello {}!, {}",name.trim_end(),greeting); //trim end will remove the newline 
 }
